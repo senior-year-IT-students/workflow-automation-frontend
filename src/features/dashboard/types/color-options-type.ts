dbc4e764d225f72f7401a313844bcc0256eacd14
YumnaAtaba3 <yumnaat33@gmail.com>
2026-02-13 17:01:03 +0300

    make  export options and to rgba  settings
    Signed-off-by: YumnaAtaba3 <yumnaat33@gmail.com>

---
+++ src/features/dashboard/types/color-options-type.ts
@@ -0,0 +1,8 @@
+ export const colorOptions = [
+  { label: "Hot Pink", value: "#EC4899" },
+  { label: "Ocean Blue", value: "#0EA5E9" },
+  { label: "Royal Purple", value: "#8B5CF6" },
+  { label: "Emerald Green", value: "#10B981" },
+  { label: "Golden Amber", value: "#F59E0B" },
+  { label: "Rose Red", value: "#F43F5E" },
+];
---
+++ src/features/dashboard/utilites/to-rgba.ts
@@ -0,0 +1,8 @@
+ export const toRgba = (hex: string, alpha: number) => {
+  const normalized = hex.replace("#", "");
+  const bigint = Number.parseInt(normalized, 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+};
